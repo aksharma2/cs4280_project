57043bc08afcb91fb74c2e7b949c155a5a7e677b
PANDA Sourav <PANDA Sourav@175.159.72.22>
2016-03-23 00:43:08 +0800

    hotel user core

---
+++ src/java/hotelBooking/core/domain/User.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hotelBooking.core.domain;
+
+/**
+ *
+ * @author PANDA Sourav
+ */
+public class User {
+    
+    private String name;
+    private String userID;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+    
+    
+}
